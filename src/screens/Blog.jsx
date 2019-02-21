cfab51bdc4e5fd6dde0a5c95a284172c7590bf14
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-21 15:11:19 +0100

    wip

--- src/screens/Blog.jsx
+++ src/screens/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 
 
 const StyledBlog = styled.div`
@@ -10,15 +10,24 @@ const StyledBlog = styled.div`
 `;
 
 const Greet = ({ name }) => <h1>hello, {name}</h1>;
+const Goodbye = ({ name }) => <h1>goodbye, {name}</h1>;
+const TakeCare = ({ name }) => <h1>take care, {name}</h1>;
 
 export default function Blog({ match }) {
   return (
     <StyledBlog>
       <h3>Blog</h3>
 
-      <Route path={`${match.url}/greet`} component={() => <Greet name='Luke' />} />
+      <Switch>
+        <Route path={`${match.url}/greet`} component={() => <Greet name='Luke' />} />
+        <Route path={`${match.url}/takecare`} component={() => <TakeCare name='Gabe' />} />
+        <Route path={`${match.url}/goodbye`} component={() => <Goodbye name='Tom' />} />
+        <Route component={() => <div>404 error</div>} />
+      </Switch>
 
-      <Link to={`${match.url}/greet`}>Vamos a gaga</Link>
+      <Link to={`${match.url}/greet`}>Greet</Link>
+      <Link to={`${match.url}/takecare`}>Take Care</Link>
+      <Link to={`${match.url}/goodbye`}>Goodbye</Link>
 
     </StyledBlog>
   );
