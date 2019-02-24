d703015bf082bbe0df8b939243e6cd834fa78a34
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-24 12:38:16 +0100

    clean up

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
-import BlackjackPlain from '../screens/Blackjack';
-import RockPaperScissorsPlain from '../screens/RockPaperScissors';
-import TicTacToePlain from '../screens/TicTacToe';
+import Blackjack from '../screens/Blackjack';
+import RockPaperScissors from '../screens/RockPaperScissors';
+import TicTacToe from '../screens/TicTacToe';
 import Section from './Section';
 
 
@@ -18,25 +18,21 @@ const StyledContainer = styled.div`
   }
 `;
 
-const Blackjack = withRouteMatching(BlackjackPlain);
-const RockPaperScissors = withRouteMatching(RockPaperScissorsPlain);
-const TicTacToe = withRouteMatching(TicTacToePlain);
-
 export default function Container() {
   return (
     <StyledContainer>
       <nav>
-        <Link to='/'>Black</Link>
-        <Link to='/rock_paper_scissors'>Rock</Link>
-        <Link to='/tic_tac_toe'>Tic</Link>
-        <Link to='/contact'>Contact</Link>
+        <a href='#'>Black</a>
+        <a href='#'>Rock</a>
+        <a href='#'>Tic</a>
+        <a href='#'>Contact</a>
       </nav>
 
-      <Blackjack path='/' />
+      <Blackjack />
 
-      <RockPaperScissors path='/rock_paper_scissors' />
+      <RockPaperScissors />
 
-      <TicTacToe path='/tic_tac_toe' />
+      <TicTacToe />
 
       <Section
         color='#d6247a'
@@ -45,40 +41,4 @@ export default function Container() {
       />
     </StyledContainer>
   );
-}
-
-function withRouteMatching(Component) {
-  return class WithRouteMatching extends React.Component {
-    state = { path: location.pathname }
-
-    setPath = () => this.setState({ path: location.pathname })
-
-    componentDidMount() {
-      addEventListener('popstate', this.setPath);
-    }
-
-    render() {
-      const pathsMatch = this.props.path === this.state.path;
-      const shouldRenderAlways = !this.props.path;
-
-      if (shouldRenderAlways || pathsMatch) {
-        return <Component {...this.props} currentPath={this.state.path} />;
-      }
-      return null;
-    }
-  };
-}
-
-class Link extends React.Component {
-  navigate = () => {
-    history.pushState(null, null, this.props.to);
-  }
-
-  render() {
-    return (
-      <a href="#" onClick={this.navigate}>
-        {this.props.children}
-      </a>
-    );
-  }
 }
