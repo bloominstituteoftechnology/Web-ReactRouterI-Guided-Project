e96c286135a790e195901b590f4966974f409c49
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-09-16 11:50:01 +0200

    wip

--- src/screens/Blackjack.jsx
+++ src/screens/Blackjack.jsx
@@ -3,9 +3,12 @@ import Section from '../components/Section';
 
 
 export default function Blackjack(props) {
+  // if we use this component with a <Route />
+  // it will get magically injected with a bunch
+  // of props we did not provide
   return (
     <Section
-      path='/'
+      path={props.location.pathname}
       color='#4286f4'
       heading='Blackjack'
       content='This is my Blackjack game.'
